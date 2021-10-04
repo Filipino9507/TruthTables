eeceadc348eb85f81c0091d84809c01600f29fa4
Filipino9507 <56320187+Filipino9507@users.noreply.github.com>
2021-10-04 12:47:03 +0200

    Setup for FunctionManager (still need testing)

--- function_manager/function_manager.py
+++ function_manager/function_manager.py
@@ -5,7 +5,7 @@ class FunctionManager:
 
     # "[NAME]:[EXPRESSION]:[VARIABLES (comma separated)]"
     FUNCTION_FORMAT = "%s:%s:%s\n"
-    BASE_PATH =  "../data/"
+    BASE_PATH = "../data/"
 
     def __init__(self, filename: str):
         self._filename = filename
@@ -14,3 +14,9 @@ class FunctionManager:
         variables = ",".join(variable_ls)
         with open(f"{self.BASE_PATH}{self._filename}", "a") as file:
             file.write(self.FUNCTION_FORMAT % (name, expression, variables))
+
+    def load_function(self, name: str) -> None:
+        # Loads function with param name
+        pass
+
+    
